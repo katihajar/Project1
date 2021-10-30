cc632f0161ece984cbf2a2ac6f3a112ed581b1ca
kati hajar <hakati1999@hotmail.com>
2021-10-30 11:40:04 +0100

    Commit

---
+++ src/main/java/com/example/project1/Dao/ComiteExecutiveDao.java
@@ -0,0 +1,4 @@
+package com.example.project1.Dao;
+
+public interface ComiteExecutiveDao {
+}
---
+++ src/main/java/com/example/project1/Service/ActiviteService.java
@@ -0,0 +1,7 @@
+package com.example.project1.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActiviteService {
+}

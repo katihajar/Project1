eab84a15d9075fe022a8e4b4cd8ddc51612bc6b7
kati hajar <hakati1999@hotmail.com>
2021-10-30 11:52:54 +0100

    Commit

---
+++ src/main/java/com/example/project1/Rest/ClubsRest.java
@@ -0,0 +1,9 @@
+package com.example.project1.Rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clubs")
+public class ClubsRest {
+}
